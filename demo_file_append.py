df1277032ba89b5dced413f350370e128ce49384
Mike Jacoby <ybocajm@gmail.com>
2020-11-30 22:02:04 -0800

    uploaded demo file
    Demo file creates mySite.html and launches in a tab in my open browser (in this case Edge)

---
+++ demo_file_append.py
@@ -0,0 +1,35 @@
+import webbrowser
+
+
+"""
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+"""
+
+"""
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile3.txt", "r")
+print(f.read())
+"""
+
+#f = open("site.html", "x")
+
+f = open("mySite.html", "w")
+
+f = open("mySite.html", "a")
+f.write("Stay tuned for our amazing summer sale!")
+f.close()
+
+f = open("mySite.html", "r")
+print(f.read())
+
+webbrowser.open("mySite.html", new=2, autoraise=True)
